9206a00325a78b575ae32eb1926c5175e6086864
Rushikesh Sonawane <70098702+Rushi-73@users.noreply.github.com>
2021-08-09 19:12:26 +0530

    Update buffer_reader.java

--- Java/buffer_reader.java
+++ Java/buffer_reader.java
@@ -6,10 +6,10 @@ class buffer_reader
 {
 	public static void main(String[] args)throws Exception {
 		
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); //creating object of class BufferedReader
 		Scanner sc=new Scanner(System.in);
 
-		int a=Integer.parseInt(br.readLine());
+		int a=Integer.parseInt(br.readLine());  //reading input from console which store character by character in inputstreamreader fetching by bufferedreader object
 		String name=br.readLine();
 		System.out.println("Integer value  "+a);
 		System.out.println("String value  "+name);
