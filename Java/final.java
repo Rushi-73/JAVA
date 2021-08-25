a55714704f37cfbc2abcd13f396894810efbe1dc
Rushikesh Sonawane <70098702+Rushi-73@users.noreply.github.com>
2021-08-25 14:19:24 +0530

    Create final.java

---
+++ Java/final.java
@@ -0,0 +1,22 @@
+//Write a java program to illustrate final in java. (Final - field, method, outer class)
+import java.util.Scanner;
+
+final class Final  //cannot inherited
+{
+	final int a=10;
+	final int b;
+	Final()
+	{
+		b=20;
+	}
+
+    final void gun() //cannot overrided
+    {
+    	System.out.println("This is final method");
+    }
+	public static void main(String[] args) {
+		
+		Final f=new Final();
+		f.gun();
+	}
+}
