6badab8997df82d92641b33afd709f71967099b0
Rushikesh Sonawane <70098702+Rushi-73@users.noreply.github.com>
2021-08-31 08:58:39 +0530

    Add files via upload

---
+++ Collection/collection.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+
+class collection
+{
+	public static void main(String[] args) {
+		
+		HashSet<Integer> hs=new HashSet<Integer>();
+
+		hs.add(10);//public boolean add(Object obj);
+
+		System.out.println("After add method "+hs);
+		HashSet<Integer> hs1=new HashSet<Integer>();
+
+		hs1.add(20);
+		hs1.add(30);
+		hs1.add(40);
+
+		hs.addAll(hs1);//public boolean addAll(Collection c);
+		System.out.println("After addAll method "+hs);
+
+		hs.remove(40);//public boolean remove(Object o);
+		System.out.println("After remove method "+hs);
+
+		hs.removeAll(hs1);//public boolean removeAll(Collection c);
+		System.out.println("After removeAll method "+hs);
+
+		hs.addAll(hs1);//public boolean addAll(Collection c);
+		System.out.println("Again After addAll method "+hs);
+
+		System.out.println("Contains method for 20 "+hs.contains(20));//public boolean contains(Object o); 
+
+ 		System.out.println("Contains all method for object "+hs.containsAll(hs1));//public boolean containsAll(Collection c);
+
+ 		hs.add(11);
+ 		hs.add(12);
+
+ 		hs.retainAll(hs1);//public boolean retainAll(Collection c);
+ 		System.out.println("After retainAll method "+hs);
+
+ 		Object[] arr=hs.toArray();//public boolean toArray();
+ 		System.out.println("After toarray method "+hs);
+
+ 		System.out.println("To check hashset is empty or not isEmpty method returns "+hs.isEmpty());//public boolean isEmpty();
+
+ 		hs.clear();
+ 		hs1.clear();//public void clear();
+
+
+
+	}
+} 
---
+++ Collection/list.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class list
+{
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> al= new ArrayList<Integer>();
+
+		al.add(0,10);//public void add(int index,Object o);
+		System.out.println("After add method on al "+al);
+
+		ArrayList<Integer> al1= new ArrayList<Integer>();
+		al1.add(0,20);
+		al1.add(1,30);
+		al1.add(2,40);
+
+		System.out.println("After add method on al1 "+al1);
+		al.addAll(1,al1);
+		System.out.println("After addAll method "+al);
+
+		al.remove(1);//public void remove(int index);
+		System.out.println("After remove method on al for 20"+al);
+
+		/*al.removeAll(al1);
+		System.out.println("After removeAll method on al for object al1"+al);*/
+
+		System.out.println("Index of 30 in list is "+al.indexOf(30));//public int indexOf(Object o);
+
+		al.set(1,22);
+		System.out.println("After set method "+al);
+		System.out.println("Get method gives object store at index 1 "+al.get(1));
+
+
+		al.add(3,10);
+
+		System.out.println("LastindexOf method gives last index of repeated number "+al.lastIndexOf(10));
+
+	}
+}
