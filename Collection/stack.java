32986fe290fb7ebbcc43c274dd488d0ef2bdad28
Rushikesh Sonawane <70098702+Rushi-73@users.noreply.github.com>
2021-09-03 09:05:19 +0530

    Create stack.java

---
+++ Collection/stack.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class stack
+{
+	public static void main(String[] args) {
+			
+			Scanner sc=new Scanner(System.in);
+
+			Stack<Integer> st=new Stack<Integer>();
+
+			st.push(10);
+			st.push(20);//public void push(Object o);
+
+			System.out.println("After push operation Stack is"+st);
+
+			System.out.println("For deleting element pop() is used it return element after deletion "+st.pop());//public object pop();
+
+			System.out.println("For getting top element in stack "+st.peek());//public object peek();
+
+			System.out.println("To search element in stack by search() "+st.search(10));//public int search(Object o);
+
+		}	
+}
