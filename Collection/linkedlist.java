86a4f2bc4613a54553ba477b11887a4fddf797e4
Rushikesh Sonawane <70098702+Rushi-73@users.noreply.github.com>
2021-09-04 21:04:32 +0530

    Create linkedlist.java

---
+++ Collection/linkedlist.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class linklist
+{
+	public static void main(String[] args) {
+		
+		LinkedList<Integer> ll=new LinkedList<Integer>();
+
+		ll.addFirst(10);//public void addFirst(Object o);
+		ll.addFirst(20);
+		System.out.println("After addfirst method linkedlist is "+ll);
+
+		ll.addLast(40);//public void addLast(Object o);
+		ll.addLast(30);
+		System.out.println("After addLast method Linkedlist is "+ll);
+
+		ll.removeFirst();//public void removeFirst();
+		System.out.println("After removing first element linkedlist is "+ll);
+
+		ll.removeLast();//public void removeLast();
+		System.out.println("After removing last element linkedlist is "+ll);
+
+
+
+	}
+}
